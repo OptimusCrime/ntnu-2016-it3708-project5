d7ca99df8ea3168d8e2410284e87517102aad37d
Kristian Ekle <kristian.ekle@gmail.com>
2016-05-09 19:39:15 +0200

    Comparator class for sorting

---
+++ src/main/java/sort/Sorter.java
@@ -0,0 +1,78 @@
+package sort;
+
+import nsga.Individual;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by krekle on 09/05/16.
+ */
+public class Sorter {
+
+
+    public static Comparator distanceComparator() {
+        return new Comparator<Individual>() {
+
+            public int compare(Individual one, Individual two) {
+                if (one.getDistance() < two.getDistance()) {
+                    return -1;
+                }
+                else if (one.getDistance() > two.getDistance()) {
+                    return 1;
+
+                }
+                else {
+                    return 0;
+                }
+            }
+        };
+    }
+
+    public static Comparator costComparator() {
+        return new Comparator<Individual>() {
+
+            public int compare(Individual one, Individual two) {
+                if (one.getCost() < two.getCost()) {
+                    return -1;
+                }
+                else if (one.getCost() > two.getCost()) {
+                    return 1;
+
+                }
+                else {
+                    return 0;
+                }
+            }
+        };
+    }
+
+    public static Comparator crowdingDistanceComparator() {
+        return new Comparator<Individual>() {
+
+            public int compare(Individual one, Individual two) {
+                if (one.getParetoRank() == two.getParetoRank()) {
+                    if (one.getCrowdingDistance() < two.getCrowdingDistance()) {
+                        return 1;
+                    }
+                    else if (one.getCrowdingDistance() > two.getCrowdingDistance()) {
+                        return -1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+                else {
+                    if (one.getParetoRank() < two.getParetoRank()) {
+                        return -1;
+                    }
+                    else {
+                        return 1;
+                    }
+                }
+            }
+        };
+    }
+
+}
